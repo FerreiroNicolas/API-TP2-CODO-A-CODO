73de35d79481baeb44d8767d1e18d9bfbe23b7ca
Nicolas Ferreiro <ferreiro.nico1@gmail.com>
2023-12-11 01:43:59 -0300

    cambio responsive

--- bemarking/static/JS/store.js
+++ bemarking/static/JS/store.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var typed = new Typed("#multiple-text", {
-        strings: ["trabajan con nosotros.", "confían en nosotros."],
-        typeSpeed: 100,
-        backSpeed: 100,
-        startDelay: 500,  
-        backDelay: 1000,
-        loop: true,
-        smartBackspace: true
-    });
+    // Verifica si estamos en un dispositivo móvil
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    // Si no estamos en un dispositivo móvil, inicializa Typed
+    if (!isMobile) {
+        var script = document.createElement('script');
+        script.src = 'https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js';
+        document.head.appendChild(script);
+
+        script.onload = function() {
+            var typed = new Typed("#multiple-text", {
+                strings: ["trabajan con nosotros.", "confían en nosotros."],
+                typeSpeed: 100,
+                backSpeed: 100,
+                startDelay: 500,
+                backDelay: 1000,
+                loop: true,
+                smartBackspace: true
+            });
+        };
+    }
 });

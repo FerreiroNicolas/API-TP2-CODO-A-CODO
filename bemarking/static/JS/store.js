a65708392924d93c658fe316672b04c245f263df
Nicolas Ferreiro <ferreiro.nico1@gmail.com>
2023-12-11 03:54:57 -0300

    otro cambio

--- bemarking/static/JS/store.js
+++ bemarking/static/JS/store.js
@@ -1,22 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-
-    if (!isMobile) {
-        var script = document.createElement('script');
-        script.src = 'https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js';
-        document.head.appendChild(script);
-
-        script.onload = function() {
-            var typed = new Typed("#multiple-text", {
-                strings: ["trabajan con nosotros.", "confían en nosotros."],
-                typeSpeed: 100,
-                backSpeed: 100,
-                startDelay: 500,
-                backDelay: 1000,
-                loop: true,
-                smartBackspace: true
-            });
-        };
-    }
+    var typed = new Typed("#multiple-text", {
+        strings: ["trabajan con nosotros.", "confían en nosotros."],
+        typeSpeed: 100,
+        backSpeed: 100,
+        startDelay: 500,  
+        backDelay: 1000,
+        loop: true,
+        smartBackspace: true
+    });
 });
